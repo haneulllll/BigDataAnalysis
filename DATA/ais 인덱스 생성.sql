b97f0720271b8b051af7f26dd11a9ce910ae999f
yeonbum <121601515+yeonbum@users.noreply.github.com>
2025-03-30 14:11:54 +0900

    ais 인덱스 생성 SQL
    ais 데이터 인덱스 생성 SQL 예시입니다.

---
+++ DATA/ais 인덱스 생성.sql
@@ -0,0 +1,11 @@
+create index idx_timestamp_2020_02 on ais_2020_02 (timestamp ASC)
+
+create index idx_timestamp_2020_03 on ais_2020_03 (timestamp ASC)
+
+create index idx_timestamp_2020_04 on ais_2020_04 (timestamp ASC)
+
+create index idx_timestamp_2020_05 on ais_2020_05 (timestamp ASC)
+
+create index idx_timestamp_2020_06 on ais_2020_06 (timestamp ASC)
+
+create index idx_timestamp_2020_07 on ais_2020_07 (timestamp ASC)
