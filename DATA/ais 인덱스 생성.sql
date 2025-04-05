699802b8f9a6e949e50377b8f6dc2d10720149f4
yeonbum <121601515+yeonbum@users.noreply.github.com>
2025-04-05 15:49:30 +0900

    Update ais 인덱스 생성.sql

--- DATA/ais 인덱스 생성.sql
+++ DATA/ais 인덱스 생성.sql
@@ -1,13 +1,15 @@
-create index idx_timestamp_2020_02 on ais_2020_02 (timestamp ASC)
+CREATE index idx_timestamp_2020_02 ON ais_2020_02 (timestamp ASC)
 
-create index idx_timestamp_2020_03 on ais_2020_03 (timestamp ASC)
+CREATE index idx_timestamp_2020_03 ON ais_2020_03 (timestamp ASC)
 
-create index idx_timestamp_2020_04 on ais_2020_04 (timestamp ASC)
+CREATE index idx_timestamp_2020_04 ON ais_2020_04 (timestamp ASC)
 
-create index idx_timestamp_2020_05 on ais_2020_05 (timestamp ASC)
+CREATE index idx_timestamp_2020_05 ON ais_2020_05 (timestamp ASC)
 
-create index idx_timestamp_2020_06 on ais_2020_06 (timestamp ASC)
+CREATE index idx_timestamp_2020_06 ON ais_2020_06 (timestamp ASC)
 
-create index idx_timestamp_2020_07 on ais_2020_07 (timestamp ASC)
+CREATE index idx_timestamp_2020_07 ON ais_2020_07 (timestamp ASC)
 
-CREATE INDEX idx_ais_mmsi_timestamp ON ais_2020_02 (mmsi, timestamp ASC);
+CREATE INDEX idx_mmsi_timestamp_02 ON ais_2020_02 (mmsi, timestamp ASC);
+
+CREATE INDEX idx_mmsi_timestamp_03 ON ais_2020_03 (mmsi, timestamp ASC);
